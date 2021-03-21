4f23072cceec8d9e718554d47d548e609be4416b
zahlulapaaja <zahlul.2001@gmail.com>
2021-03-21 14:14:20 +0700

    tambah laman profil

---
+++ application/controllers/admin/Profil.php
@@ -0,0 +1,25 @@
+<?php
+class Profil extends CI_Controller
+{
+	function __construct()
+	{
+		parent::__construct();
+		if ($this->session->userdata('masuk') != TRUE) {
+			$url = base_url();
+			redirect($url);
+		};
+		$this->load->model('m_kategori');
+		$this->load->model('m_barang');
+	}
+	function index()
+	{
+		if ($this->session->userdata('akses') == '1') {
+			$data['data'] = $this->m_barang->tampil_barang();
+			$data['kat'] = $this->m_kategori->tampil_kategori();
+			$data['kat2'] = $this->m_kategori->tampil_kategori();
+			$this->load->view('admin/v_profil', $data);
+		} else {
+			echo "Halaman tidak ditemukan";
+		}
+	}
+}
--- application/views/admin/v_index2.php
+++ application/views/admin/v_index2.php
@@ -164,7 +164,7 @@
             <!-- Brand Logo -->
             <a href="index3.html" class="brand-link">
                 <img src="<?php echo base_url() . 'assets/adminlte/dist/img/AdminLTELogo.png' ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">AdminLTE 3</span>
+                <span class="brand-text font-weight-light">STIS Point of Sales</span>
             </a>
 
             <!-- Sidebar -->
@@ -175,7 +175,7 @@
                         <img src="<?php echo base_url() . 'assets/adminlte/dist/img/user2-160x160.jpg' ?>" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="<?php echo base_url() . 'admin/profil' ?>" class="d-block">Zahlul Fuadi</a>
                     </div>
                 </div>
 
