80a85617d52050b9bc258e2a1860ae0b91592bb2
zahlulapaaja <zahlul.2001@gmail.com>
2021-03-30 06:36:21 +0700

    dashboard ala kadar

--- application/views/layout/sidebar.php
+++ application/views/layout/sidebar.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="<?php echo base_url() . 'welcome' ?>" class="brand-link">
-        <img src="<?php echo base_url() . 'assets/img/logopos.png' ?>" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+        <img src="<?php echo base_url() . 'assets/img/logopos.png' ?>" alt="STIS Point of Sale" class="brand-image img-circle elevation-3" style="opacity: .8">
         <span class="brand-text font-weight-light">STIS Point of Sales</span>
     </a>
 
